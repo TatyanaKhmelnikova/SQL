b3102324964e6a17710a756d519ff49e6e9ff271
Tatyana <96234467+TatyanaKhmelnikova@users.noreply.github.com>
2023-06-11 08:59:45 +0300

    ДЗ-4

---
+++ profiles.sql
@@ -0,0 +1,100 @@
+DROP DATABASE IF EXISTS lesson_4_db;
+CREATE DATABASE lesson_4_db;
+USE lesson_4_db;
+
+-- пользователи
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY, -- SERIAL = BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE
+    firstname VARCHAR(50),
+    lastname VARCHAR(50) COMMENT 'Фамилия',
+    email VARCHAR(120) UNIQUE
+);
+
+INSERT INTO users (id, firstname, lastname, email) VALUES 
+(1, 'Reuben', 'Nienow', 'arlo50@example.org'),
+(2, 'Frederik', 'Upton', 'terrence.cartwright@example.org'),
+(3, 'Unique', 'Windler', 'rupert55@example.org'),
+(4, 'Norene', 'West', 'rebekah29@example.net'),
+(5, 'Frederick', 'Effertz', 'von.bridget@example.net'),
+(6, 'Victoria', 'Medhurst', 'sstehr@example.net'),
+(7, 'Austyn', 'Braun', 'itzel.beahan@example.com'),
+(8, 'Jaida', 'Kilback', 'johnathan.wisozk@example.com'),
+(9, 'Mireya', 'Orn', 'missouri87@example.org'),
+(10, 'Jordyn', 'Jerde', 'edach@example.com');
+
+
+
+-- пользователи сообщества
+DROP TABLE IF EXISTS users_communities;
+CREATE TABLE users_communities(
+	user_id BIGINT UNSIGNED NOT NULL,
+	community_id BIGINT UNSIGNED NOT NULL,
+    PRIMARY KEY (user_id, community_id),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (community_id) REFERENCES communities(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+INSERT INTO users_communities (user_id, community_id) 
+VALUES 
+(1, 1), (1, 2), (1, 3),
+(2, 1), (2, 2), 
+(3, 1), (3, 2), (3, 10), (3, 5), (3, 8),
+(4, 1), (4, 2), (4, 3), (4, 8),
+(5, 1), (5, 2), (5, 3), (5, 6), (5, 8), (5, 10),
+(6, 1), (6, 2), (6, 3), (6, 6),
+(7, 1), (7, 2), (7, 3), (7, 8), (7, 7), (7, 6),
+(8, 1), (8, 2), (8, 3), (8, 5), (8, 7), (8, 9),
+(9, 1), (9, 2),  
+(10, 1), (10, 10);
+
+
+
+-- лайки медиа
+DROP TABLE IF EXISTS likes;
+CREATE TABLE likes(
+	id SERIAL PRIMARY KEY,
+    user_id BIGINT UNSIGNED NOT NULL,
+    media_id BIGINT UNSIGNED NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)  ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (media_id) REFERENCES media(id)  ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+INSERT INTO likes (user_id, media_id) VALUES 
+(1, 1), (2, 1), (3, 1), (4, 1), (5, 2), (6, 2), (7, 2), (8, 8), (1, 9), (10, 15), (7, 11), (5, 12), (6, 13), (1, 14), (1, 15), (1, 16), (1, 17), (1, 18), (1, 19), (1, 20); 
+
+-- профиль пользователя
+DROP TABLE IF EXISTS `profiles`;
+CREATE TABLE `profiles` (
+	user_id SERIAL PRIMARY KEY,
+    gender CHAR(1),
+    birthday DATE,
+	photo_id BIGINT UNSIGNED,
+    hometown VARCHAR(100),
+    FOREIGN KEY (user_id) REFERENCES users(id)  ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (photo_id) REFERENCES media(id)  ON UPDATE CASCADE ON DELETE SET NULL
+);
+INSERT INTO `profiles` (user_id, gender, birthday, photo_id, hometown) VALUES 
+(1, 'f', '1976-11-08', 9, 'Adriannaport'),
+(2, 'f', '2011-12-20', 13, 'North Nettieton'),
+(3, 'm', '1994-06-15', 3, 'Padbergtown'),
+(4, 'f', '1979-11-07', NULL, 'Port Rupertville'),
+(5, 'f', '1976-04-19', 5, 'Spencerfort'),
+(6, 'f', '1983-09-07', 6, 'South Woodrowmouth'),
+(7, 'm', '2014-07-31', NULL, 'South Jeffereyshire'),
+(8, 'f', '1975-03-26', 17, 'Howeside'),
+(9, 'f', '1982-11-01', 9, 'Loweborough'),
+(10, 'm', '1977-05-14', NULL, 'New Nellaside'); 
+
+
+-- Подсчитать общее количество лайков, которые получили пользователи
+-- младше 12 лет.
+SELECT COUNT(*) as 'Likes' FROM profiles WHERE (YEAR(NOW())-YEAR(birthday)) < 12;
+
+-- Определить кто больше поставил лайков (всего): мужчины или
+-- женщины.
+SELECT gender, COUNT(*) AS 'Кол-во' FROM profiles GROUP BY gender;
+
+
+ 
+ 
